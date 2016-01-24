b99bfa78130b15b8144c9060f9995c5fe0f46599
Zvonimir Pišković <piskovic.zvonimir@gmail.com>
2016-01-24 13:34:29 +0100

    modified templates to display post from db

--- alfaApp/views.py
+++ alfaApp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Defect
 
 def defect_list(request):
-    return render(request, 'alfaApp/defect_list.html', {})
+    defects = Defect.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'alfaApp/defect_list.html', {'defects':defects})
