5d0842d0b32e2a0cebe8f315c1ddafb63debf058
Zvonimir Pišković <piskovic.zvonimir@gmail.com>
2016-01-24 14:42:13 +0100

    added view and template for ditailed defect report, added css

--- alfaApp/urls.py
+++ alfaApp/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.defect_list, name='defect_list'),
+    url(r'^defect/(?P<pk>[0-9]+)/$', views.defect_detail, name='defect_detail'),
 ]
--- alfaApp/views.py
+++ alfaApp/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Defect
 
 def defect_list(request):
     defects = Defect.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'alfaApp/defect_list.html', {'defects':defects})
+
+def defect_detail(request, pk):
+    defect = get_object_or_404(Defect, pk=pk)
+    return render(request, 'alfaApp/defect_detail.html', {'defect': defect})
