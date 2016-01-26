fe8ee795292a3526575ab0be5f2536be9e4b11c4
Zvonimir Pišković <piskovic.zvonimir@gmail.com>
2016-01-26 01:52:29 +0100

    login st step

--- alfaApp/urls.py
+++ alfaApp/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^login/$', views.login_user, name = 'login_user'),
     url(r'^$', views.welcome_page, name='welcome_page'),
     url(r'^defect/list$', views.defect_list, name='defect_list'),
     url(r'^defect/(?P<pk>[0-9]+)/$', views.defect_detail, name='defect_detail'),
--- alfaApp/views.py
+++ alfaApp/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.http import *
+from django.shortcuts import render, render_to_response, redirect, get_object_or_404
 from django.utils import timezone
 from .models import Defect
 from .forms import PostForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.template import RequestContext
 
 def welcome_page(request):
     return render(request, 'alfaApp/welcome_page.html', {})
 
+def login_user(request):
+    logout(request)
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/defect/list')
+    return render_to_response('registration/login.html', context_instance=RequestContext(request))
+
+@login_required(login_url='/login/')
 def defect_list(request):
     defects = Defect.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'alfaApp/defect_list.html', {'defects':defects})
